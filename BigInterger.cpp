8c1d10cb4ffe52bbba5d443556070b0d266b1fe2
Sakib Sami <s4kibs4mi@gmail.com>
2014-02-10 04:10:51 +0700

    Create BigInterger.cpp

---
+++ BigInterger.cpp
@@ -0,0 +1,47 @@
+// Interger Class for performing Big Integer Taskes
+
+#include<iostream>
+#include<string>
+#include<cstdio>
+#include<cstdlib>
+#include<algorithm>
+
+using namespace std;
+
+class Integer{
+	private:
+		string c;
+	public:
+		string Add(string a,string b){
+			reverse(a.begin(),a.end());
+			reverse(b.begin(),b.end());
+			if(a.length() < b.length())
+				swap(a,b);
+			while(int(b.length()) <= int(a.length()))
+				b.push_back('0');
+				
+			int i = 0,carry = 0;
+			while(a[i]){
+				carry = carry + a[i]-48 + b[i]-48;
+				c.push_back((carry %10) + 48);
+				carry = carry / 10;
+				i++;
+			}
+			while(carry > 10){
+				c.push_back((carry % 10) + 48);
+				carry = carry / 10;
+			}
+			if(carry != 0) c.push_back(carry + 48);
+			reverse(c.begin(),c.end());
+			return c;
+		}
+	
+};
+
+int main(){
+	string a,b;
+	Integer Int;
+	cin >> a >> b;
+	cout << Int.Add(a,b)<< endl;
+	return 0;
+}
