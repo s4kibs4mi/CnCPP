f5f1a38afc7b924b099cff838e79c582d972b8b2
Sakib Sami <s4kibs4mi@gmail.com>
2014-04-04 00:44:41 +0600

    Update BigInterger.cpp

--- BigInterger.cpp
+++ BigInterger.cpp
@@ -1,244 +1,102 @@
-// [+] Interger Class for performing Big Integer Taskes such as Addition,Substruction,Multiplication,Division etc
-// [+] And some usefull Functions
-// [+] Coded in C++ using C++ Standard library
-// [+] Coding : Sakib Sami
-// [+] Email : s4kibs4mi@gmail.com
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
-#include<iostream>
-#include<string>
-#include<cstdio>
-#include<cstdlib>
-#include<algorithm>
-#include<vector>
+#ifndef BIGINTEGER_H
+#define BIGINTEGER_H
 
 using namespace std;
 
-class Integer{
-	private:
-		string c;
-	public:
-		// Method to check within Two Integer number which is Big or same
-		string BigIntFind(string a,string b){
-			if(a.length() < b.length()) swap(a,b);
-			string d = b;
-			reverse(b.begin(),b.end());
-			while(b.length() < a.length()) b.push_back('0');
-			reverse(b.begin(),b.end());
-			int i = 0;
-			while(a[i]){
-				if(a[i] > b[i]) return a;
-				else if(a[i] < b[i]) return d;
-				i++;
-			}
-			return "same";
-		}
+/** BigInteger Library
+    Coding : Sakib Sami ( s4kibs4mi@gmail.com )
+    Coded in Standard C++
+    implemented functions :
+    addition,substraction,fibonacci numbers
+    to work with this add "biginteger.h" in your code
+*/
 
-		// Method to make power
-		string Power(long int base,long int power){
-			string res = "1";
-			while(power--){
-				res = M(res,base);
-			}
-			return res;
-		}
-		
-		// Method to Findout Digits of a number
-		long int Digit(long int n){
-			long int i = 0;
-			for(;n > 0; i++) n /= 10;
-			return i;
-		}
+class BigInteger{
+public:
+    // method to findout the greater number
+    int BigIntFind(string a,string b){
+        if(a.length() < b.length()) swap(a,b);
+        string d = b;
+        reverse(b.begin(),b.end());
+        while(b.length() < a.length()) b.push_back('0');
+        reverse(b.begin(),b.end());
+        int i = 0;
+        while(a[i]){
+            if(a[i] > b[i]) return -1;
+            else if(a[i] < b[i]) return 1;
+            i++;
+        }
+        return 0;
+    }
 
-		// Method to Convert string to number
-		long int s2li(string a){
-			long int n = 0;
-			reverse(a.begin(),a.end());
-			for(int i=0; a[i]; i++) n = ( n*10 ) + (a[i]-48);
-			return n;
-		}
+    // method for addition
+    string addition(string a,string b){
+        string c;
+        reverse(a.begin(),a.end());
+        reverse(b.begin(),b.end());
+        if(a.length() < b.length())
+            swap(a,b);
+        while(int(b.length()) < int(a.length()))
+            b.push_back('0');
 
-		// Method to Convert number to string
-		string li2s(long int a){
-			c.clear();
-			if(a==0){ c.push_back(a+48); return c; }
-			while(a > 0){
-				c.push_back((a%10)+48);
-				a /= 10;
-			}
-			reverse(c.begin(),c.end());
-			return c;
-		}
+        int i = 0,carry = 0;
+        while(a[i]){
+            carry = carry + a[i]-48 + b[i]-48;
+            c.push_back((carry %10) + 48);
+            carry = carry / 10;
+            i++;
+        }
+        while(carry > 9){
+            c.push_back((carry % 10) + 48);
+            carry = carry / 10;
+        }
+        if(carry != 0) c.push_back(carry + 48);
+        reverse(c.begin(),c.end());
+        return c;
+    }
 
-		// Method to Add two Integer number
-		string Add(string a,string b){
-			c.clear();
-			reverse(a.begin(),a.end());
-			reverse(b.begin(),b.end());
-			if(a.length() < b.length())
-				swap(a,b);
-			while(int(b.length()) < int(a.length()))
-				b.push_back('0');
+    // method for substraction
+    string substract(string a,string b){
+        string c;
+        bool flag = true;
+        if(BigIntFind(a,b)==1){
+            swap(a,b);
+            flag = false;
+        }
+        reverse(a.begin(),a.end());
+        reverse(b.begin(),b.end());
+        while(b.length() < a.length()) b.push_back('0');
+        int i = 0,carry = 0,x = 0;
+        while(a[i]){
+            if(b[i] > a[i]) x = (a[i]-48) + 10;
+            else x = a[i]-48;
+            carry = x-(carry + (b[i]-48));
+            c.push_back(carry+48);
+            carry = x / 10;
+            i++;
+        }
+        while(c[c.length()-1]=='0' && c.length() > 1) c.erase(c.length()-1,1);
+        if(!flag) c.push_back('-');
+        reverse(c.begin(),c.end());
+        return c;
+    }
 
-			int i = 0,carry = 0;
-			while(a[i]){
-				carry = carry + a[i]-48 + b[i]-48;
-				c.push_back((carry %10) + 48);
-				carry = carry / 10;
-				i++;
-			}
-			while(carry > 9){
-				c.push_back((carry % 10) + 48);
-				carry = carry / 10;
-			}
-			if(carry != 0) c.push_back(carry + 48);
-			reverse(c.begin(),c.end());
-			return c;
-		}
-
-		// Method to Substruct within two Integer number
-		string Minus(string a,string b){
-			c.clear();
-			bool flag = true;
-			if(BigIntFind(a,b)==b){
-				swap(a,b);
-				flag = false;
-			}
-			reverse(a.begin(),a.end());
-			reverse(b.begin(),b.end());
-			while(b.length() < a.length()) b.push_back('0');
-			int i = 0,carry = 0,x = 0;
-			while(a[i]){
-				if(b[i] > a[i]) x = (a[i]-48) + 10;
-				else x = a[i]-48;
-				carry = x-(carry + (b[i]-48));
-				c.push_back(carry+48);
-				carry = x / 10;
-				i++;
-			}
-			while(c[c.length()-1]=='0' && c.length() > 1) c.erase(c.length()-1,1);
-			if(!flag) c.push_back('-');
-			reverse(c.begin(),c.end());
-			return c;
-		}
-
-		// Method for multiplication
-		string M(string a,long int b){
-			c.clear();
-			long int m = 0,i;
-			reverse(a.begin(),a.end());
-			for(i = 0; a[i] ; i++){
-				m = m + b * (a[i]-'0');
-				c.push_back((m%10) + 48);
-				m /= 10;
-			}
-			while(m > 0){
-				c.push_back((m%10) + 48);
-				m /= 10;
-			}
-			reverse(c.begin(),c.end());
-			return c;
-		}
-
-		// Method for division
-		string D(char ch,string a,long int b){
-			c.clear();
-			long int Len = a.length(),mod = 0,d = Digit(b),V = 0,i;
-			i = 0;
-			while(i < d || V < b){ V = (V * 10) + (a[i]-48); i++; }
-			while(i < Len +1){
-
-				c.push_back((V/b)+48);
-				mod = V % b;
-				V = (mod*10) + (a[i]-48);
-				if(b > V) { i++; V = (V*10) + (a[i]-48); if(i!=Len+1) c.push_back('0'); }
-				i++;
-			}
-			if(c.empty()) c = "0";
-			if(ch=='m') c = li2s(mod);
-			return c;
-		}
-
-		// Method to Generate Fibonacci numbers
-		void F(string a,string b,long unsigned int n){
-	    	if(n){
-				cout << a << endl;
-				c = Add(a,b);
-				F(b,c,--n);
-			}
-		}
-		
-		// Method to Convert number from decimal to any other base
-		string D2All(string temp,long int base){
-			string conv;
-			bool flag;
-			long int numb,res,i;
-			if(temp=="1") return "1";
-			if(temp=="0") return "0";
-			while(s2li(temp) >= base){
-				numb = 0, res = 0, i = 0;
-				while(numb <= base && temp[i])
-					numb = (numb*10) + (temp[i++]-48);
-				while(temp[i] || numb >= base){
-					res = (res*10) + (numb/base);
-					numb = numb%base;
-					flag = false;
-					while(numb <= base && temp[i]){
-						numb = (numb*10) + (temp[i++]-48);
-						if(flag) res = (res*10);
-						flag = true;
-					}
-				}
-				if(flag && numb < base)
-					res = (res*10);
-				if(numb >= 0 && numb <= 9)
-					conv.push_back(numb+48);
-				else
-					conv.push_back((numb%10)+65);
-				temp = li2s(res);
-			}
-			if(res != 0 && res < base){
-				if(res >= 0 && res <= 9)
-					conv.push_back(res+48);
-				else
-					conv.push_back((res%10)+65);
-			}
-			reverse(conv.begin(),conv.end());
-			return conv;
-		}
-		
-		// Method to Convert number from Any Base to Decimal
-		string All2D(string temp, long int base){
-			string dec;
-			long int i;
-			reverse(temp.begin(),temp.end());
-			i = 0;
-			while(temp[i]){
-				if(temp[i] != '0'){
-					if(temp[i] >= 65){
-						dec = Add(dec,M(Power(base,i),((temp[i]%65)+10)));
-					}
-					else{
-						dec = Add(dec,M(Power(base,i),((temp[i]-48))));
-					}
-				}
-				i++;
-			}
-			return dec;
-		}
-		
-		// Method to Check Palindrome
-		bool IsPalindrome(string s){
-			long int i, n = s.length(),n2;
-			n2 = n/2;
-			for(i = 0; i <= n2; i++){
-				if(s[i] != s[n-i-1])
-					return false;
-			}
-			return true;
-		}
+    // method to generate fibonacci numbers
+    vector <string> F(string a,string b,long unsigned int n){
+        vector <string> f;
+        string c;
+        if(n){
+            f.push_back(a);
+            c = addition(a,b);
+            F(b,c,--n);
+        }
+        return f;
+    }
 };
 
-int main(){
-	
-	return 0;
-}
+#endif // BigInteger_H
